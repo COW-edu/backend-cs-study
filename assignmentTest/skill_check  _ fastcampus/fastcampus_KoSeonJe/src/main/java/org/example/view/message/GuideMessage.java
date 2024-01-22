8f8dedf447b98b4d82ceb751fe9f5a3017a531f9
koseonje <kosunje1455@navet.com>
2024-01-23 01:24:59 +0900

    feat: 가이드 메시지 생성

---
+++ assignmentTest/skill_check  _ fastcampus/fastcampus_KoSeonJe/src/main/java/org/example/view/message/GuideMessage.java
@@ -0,0 +1,9 @@
+package org.example.view.message;
+
+public class GuideMessage {
+
+  public final static String SELECT_NUMBER = "번호를 입력해 주세요 :";
+  public final static String PROCESS_OPTION = "1.계좌 생성 / 2.계좌 정보 조회 / 3.출금 / 4.입금 / 5.송금";
+  public final static String ACCOUNT_OPTION = "1.예금 계좌 / 2.적금 계좌";
+
+}
