c7141be4800e6abb208fe032782b3d8a7054495a
koseonje <kosunje1455@navet.com>
2024-01-23 01:25:36 +0900

    feat: 뱅크 매니저(컨트롤), 뱅크 서비스 인터페이스 구현

---
+++ assignmentTest/skill_check  _ fastcampus/fastcampus_KoSeonJe/src/main/java/org/example/bank/service/BankService.java
@@ -0,0 +1,9 @@
+package org.example.bank.service;
+
+public interface BankService {
+  void createBankAccount();
+  void checkAccountInfo();
+  void withdraw();
+  void deposit();
+  void transfer();
+}
